eab9bc96f0680f58b898f7a4cba2d5021df24632
maximeCony <maxime.cony@gmail.com>
2014-11-18 12:57:20 -0800

    more logic

--- app.js
+++ app.js
@@ -3,7 +3,14 @@
 var express = require('express'),
   app = express(),
   http = require('http').Server(app),
-  io = require('socket.io')(http);
+  io = require('socket.io')(http),
+  video = {
+    users: {},
+    // paused: true,
+    startedAt: null,
+    at: null,
+  },
+  allowedDifference = 0.7;
 
 app.use('/public', express.static(__dirname + '/public'));
 
@@ -11,19 +18,35 @@ app.get('/', function(req, res) {
   res.sendfile('./index.html');
 });
 
+var requireUpdate = function(serverDifference, clientDifference) {
+  var max = Math.max(serverDifference, clientDifference),
+    min = Math.min(serverDifference, clientDifference),
+    difference = max - min;
+  return difference > allowedDifference;
+};
+
 io.on('connection', function(socket) {
-  console.log('a user connected');
+  video.users[socket.id] = {
+    socket: socket,
+  };
+  var user = video.users[socket.id];
   socket.on('disconnect', function() {
-    console.log('user disconnected');
+    delete video.users[socket.id];
   });
-  socket.on('play', function() {
-    socket.broadcast.emit('play');
+  socket.on('play', function(params) {
+    video.startedAt = params.timestamp;
+    video.at = params.at;
+    socket.broadcast.emit('play', params.at);
   });
   socket.on('pause', function() {
     socket.broadcast.emit('pause');
   });
-  socket.on('timeupdate', function(at) {
-    socket.broadcast.emit('timeupdate', at);
+  socket.on('timeupdate', function(params) {
+    var serverDifference = (video.startedAt / 1000) - video.at,
+      clientDifference = (params.timestamp / 1000) - params.at;
+    if (requireUpdate(serverDifference, clientDifference)) {
+      user.socket.emit('updatetime', params.at);
+    }
   });
 });
 
--- public/app.js
+++ public/app.js
@@ -2,36 +2,51 @@
 
 var socket = io(),
   $video = $('#js-video'),
-  allowedDifference = 0.6,
-  emitTimeUpdateInterval;
+  allowedDifference = 0.7,
+  emitPlay = true,
+  emitPause = true;
 
-var emitTimeUpdate = function() {
-  socket.emit('timeupdate', $video[0].currentTime);
+var updateCurrentTime = function(at) {
+  var currentTime = $video[0].currentTime,
+    difference = Math.max(currentTime, at) - Math.min(currentTime, at);
+  if (difference > allowedDifference) {
+    $video[0].currentTime = at;
+  }
 };
 
 $video
   .on('play', function() {
-    socket.emit('play');
-    clearInterval(emitTimeUpdateInterval);
-    emitTimeUpdateInterval = setInterval(emitTimeUpdate, 1500);
+    if (!emitPlay) return emitPlay = true;
+    var params = {
+      at: $video[0].currentTime, 
+      timestamp: Date.now(),
+    };
+    socket.emit('play', params);
   })
   .on('pause', function() {
+    if (!emitPause) return emitPause = true;
     socket.emit('pause');
-    clearInterval(emitTimeUpdateInterval);
+  })
+  .on('timeupdate', function() {
+    var params = {
+      at: $video[0].currentTime, 
+      timestamp: Date.now(),
+    };
+    socket.emit('timeupdate', params);
   });
 
 socket
-  .on('play', function() {
+  .on('play', function(at) {
+    // prevent to re-emit play
+    updateCurrentTime(at);
+    emitPlay = false;
     $video[0].play();
   })
   .on('pause', function() {
+    // prevent to re-emit pause
+    emitPause = false;
     $video[0].pause();
   })
-  .on('timeupdate', function(at) {
-    if ($video[0].seeking) return;
-    var currentTime = $video[0].currentTime,
-      difference = Math.max(currentTime, at) - Math.min(currentTime, at);
-    if (difference > allowedDifference) {
-      $video[0].currentTime = at;
-    }
+  .on('updatetime', function(at) {
+    updateCurrentTime(at);
   });
